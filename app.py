2e461c35c7caaa7ddbd03fe95faf0a7e3ad6b883
Kelechi Nwachukwu <caesey2001@yahoo.com>
2021-02-09 20:47:24 -0500

    feat: Add web application

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    lines = ["<h1>Hello World!</h1>"]
+    if os.environ.get('MY_ENV_VAR'):
+        lines.append(os.environ.get('MY_ENV_VAR'))
+
+    return '\n'.join(lines)
+
+
+if __name__ == "__main__":
+    app.run()
