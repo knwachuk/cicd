6832d4e1d5e315ab32fded7ccb5b7df4af090848
Kelechi Nwachukwu <caesey2001@yahoo.com>
2021-02-10 12:22:05 -0500

    refactor: Add wsgi

--- app.py
+++ app.py
@@ -8,11 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     lines = ["<h1>Hello World!</h1>"]
-    if os.environ.get('MY_ENV_VAR'):
-        lines.append(os.environ.get('MY_ENV_VAR'))
+    if os.environ.get("MY_ENV_VAR"):
+        lines.append(os.environ.get("MY_ENV_VAR"))
 
-    return '\n'.join(lines)
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000) #
+    return "\n".join(lines)
